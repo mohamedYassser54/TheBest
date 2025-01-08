5c67da40751892f51b5177d6f6a4df550688c0bb
mohamed yasser <mohamedyasser12346262@gmail.com>
2025-01-08 21:01:44 +0200

    update3

--- frontend/src/component/cards/data.js
+++ frontend/src/component/cards/data.js
@@ -1,7 +1,59 @@
 import React from "react";
-import img from './images/555.png'
-import img2 from './images/2.png'
 
+import img1 from './images/1.jpg'
+import img2 from './images/2.jpg'
+import img3 from './images/3.jpg'
+import img4 from './images/4.jpg'
+import img5 from './images/5.jpg'
+
+
+import img6 from './images/6.jpg'
+import img7 from './images/7.jpg'
+import img8 from './images/8.jpg'
+import img9 from './images/9.jpg'
+import img10 from './images/10.jpg'
+
+
+import img11 from './images/11.jpg'
+import img12 from './images/12.jpg'
+import img13 from './images/13.jpg'
+import img14 from './images/14.jpg'
+import img15 from './images/15.jpg'
+
+
+import img16 from './images/16.jpg'
+import img17 from './images/17.jpg'
+import img18 from './images/18.jpg'
+import img19 from './images/19.jpg'
+import img20 from './images/20.jpg'
+
+
+import img21 from './images/21.jpg'
+import img22 from './images/22.jpg'
+import img23 from './images/23.jpg'
+import img24 from './images/24.jpg'
+import img25 from './images/25.jpg'
+
+
+import img26 from './images/26.jpg'
+import img27 from './images/27.jpg'
+import img28 from './images/28.jpg'
+import img29 from './images/29.jpg'
+import img30 from './images/30.jpg'
+
+
+import img31 from './images/31.jpg'
+import img32 from './images/32.jpg'
+import img33 from './images/33.jpg'
+import img34 from './images/34.jpg'
+import img35 from './images/35.jpg'
+
+
+import img36 from './images/36.jpg'
+import img37 from './images/37.jpg'
+import img38 from './images/38.jpg'
+import img39 from './images/39.jpg'
+import img40 from './images/40.jpg'
 
 
 
@@ -9,41 +61,268 @@ import img2 from './images/2.png'
 const data=[
     {
     id:1,
-    img:img,
-    name:"Tamer Hosny",
-    Description:"مطرب مصري",
+    img:img1,
+    name:"عمر خضر ",
+    Description:"نادى استون فيلا الانجليزى ",
     votes: 0
     },
     {
     id:2,
     img:img2,
-    name:"Ahmed ElGend",
-    Description:"رياضي ",
+    name:"بلال عطية ",
+    Description:"النادى الاهلى ",
     votes: 0
     },
     {
     id:3,
-    img:img,
-    name:"Tamer Hosny",
-    Description:"مطرب مصري"
+    img:img3,
+    name:"حازم اسامة علي عبد العزيز",
+    Description:"الزمالك"
     },
     {
-    id:1,
-    img:img2,
-    name:"Ahmed ElGend",
-    Description:"رياضي"
+    id:4,
+    img:img4,
+    name:"عمر محمد عبدالسلام الشبراوى ",
+    Description:"نادى حرس الحدود "
     },
     {
-    id:1,
-    img:img,
-    name:"Tamer Hosny",
-    Description:"مطرب مصري"
+    id:5,
+    img:img5,
+    name:"عمرو خالد بيبو ",
+    Description:"نادى اراو "
+    },
+
+
+
+
+
+    {
+    id:6,
+    img:img6,
+    name:"محمد السيد ",
+    Description:"نادى الزمالك "
     },
     {
-    id:1,
-    img:img2,
-    name:"Ahmed ElGend",
-    Description:"رياضي"
+    id:7,
+    img:img7,
+    name:"عمر فتحي عمورى",
+    Description:"نادى الاتحاد السكندري "
     },
+    {
+    id:8,
+    img:img8,
+    name:"عمار احمد عبد السلام",
+    Description:"نادى الإسماعيلي"
+    },
+    {
+    id:9,
+    img:img9,
+    name:"محمد سعيد حسن عثمان",
+    Description:"نادى سموحة "
+    },
+    {
+    id:10,
+    img:img10,
+    name:"عبد المنعم تامر ",
+    Description:"نادى زد "
+    },
+
+
+
+
+    {
+    id:11,
+    img:img11,
+    name:"احمد فوزي ",
+    Description:"نادى بيراميدز "
+    },
+    {
+    id:12,
+    img:img12,
+    name:"اياد صلاح ابراهيم احمد عاشور ",
+    Description:"نادي المقاولون العرب"
+    },
+    {
+    id:13,
+    img:img13,
+    name:"محمد احمد زعلوك ",
+    Description:"النادى الاهلي "
+    },
+    {
+    id:14,
+    img:img14,
+    name:"انس محمد عبدالرحمن علي",
+    Description:"نادى فيوتشر"
+    },
+    {
+    id:15,
+    img:img15,
+    name:"محمد حسام مسعد محمد",
+    Description:"الاوليمبي السكندري"
+    },
+
+
+
+    {
+    id:16,
+    img:img16,
+    name:"فاروق احمد فاروق علي ",
+    Description:" غزل المحله  "
+    },
+    {
+    id:17,
+    img:img17,
+    name:"محمد اشرف جمال الدين ",
+    Description:" حرس الحدود"
+    },
+    {
+    id:18,
+    img:img18,
+    name:"احمد ايهاب محمد عبد الغفار",
+    Description:" سموحة"
+    },
+    {
+    id:19,
+    img:img19,
+    name:"احمد خالد جمعة سليمان ",
+    Description:"  الاهلي "
+    },
+    {
+    id:20,
+    img:img20,
+    name:"احمد ابراهيم محمد ابراهيم درويش",
+    Description:" الرجاء المطروحي"
+    },
+
+
+
+    {
+    id:21,
+    img:img21,
+    name:"ابراهيم احمد ابراهيم موسي ",
+    Description:"نادي حرس الحدود "
+    },
+    {
+    id:22,
+    img:img22,
+    name:"خالد ربيع محمد ابراهيم ",
+    Description:"نادي أبوقير للاسمده "
+    },
+    {
+    id:23,
+    img:img23,
+    name:"انس وائل ",
+    Description:"نادى الزمالك "
+    },
+    {
+    id:24,
+    img:img24,
+    name:"ادهم محمد إسماعيل ",
+    Description:"نادي توماس النوبي "
+    },
+    {
+    id:25,
+    img:img25,
+    name:"محمد احمد عبداللطيف محمد",
+    Description:"نادى نجوم الحضرة "
+    },
+
+
+
+    {
+    id:26,
+    img:img26,
+    name:"زياد وائل الشحات جبر ",
+    Description:" مصري السلوم"
+    },
+    {
+    id:27,
+    img:img27,
+    name:" احمد عادل صبره محمد ",
+    Description:" الحريه المطروحي  "
+    },
+    {
+    id:28,
+    img:img28,
+    name:"محمد خميس حسن محمد ",
+    Description:" شباب الناصرية "
+    },
+    {
+    id:29,
+    img:img29,
+    name:"احمد السيد عبد الفتاح بدر ",
+    Description:" بيلا "
+    },
+    {
+    id:30,
+    img:img30,
+    name:"صلاح الدين قدري حسن",
+    Description:" الحمام المطروحي"
+    },
+
+
+    {
+    id:31,
+    img:img31,
+    name:"حسن خلف عبداللاه احمد",
+    Description:" لافينيا "
+    },
+    {
+    id:32,
+    img:img32,
+    name:"على فرغلي ",
+    Description:" المنتصر "
+    },
+    {
+    id:33,
+    img:img33,
+    name:"احمد محمود اسعد ابراهيم",
+    Description:" العامرية الرياضي"
+    },
+    {
+    id:34,
+    img:img34,
+    name:"محمد سعيد حسن الحوفي ",
+    Description:" توماس النوبي "
+    },
+    {
+    id:35,
+    img:img35,
+    name:"محمد مصطفى محمد رضا علي",
+    Description:" التعاون "
+    },
+
+    
+    {
+    id:36,
+    img:img36,
+    name:"اياد محمد جاد الرب",
+    Description:" الرياضي البحري"
+    },
+    {
+    id:37,
+    img:img37,
+    name:"محمد السيد محمد السيد",
+    Description:" شباب قنا "
+    },
+    {
+    id:38,
+    img:img38,
+    name:"زياد وائل الشحات جبر ",
+    Description:" مصري السلوم"
+    },
+    {
+        id:39,
+        img:img39,
+        name:"محمد مصطفى محمد رضا علي ",
+        Description:" التعاون "
+        },
+    {
+    id:40,
+    img:img40,
+    name:"عبد الله شعبان فتحي نجيب زناتي",
+    Description:"  الورديان الرياضي"
+    },
+   
 ]
 export default data;
